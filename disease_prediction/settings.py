20e3e8c435897fa211667b5a2eb536fd2fb2da28
Jayesh Baviskar <jayeshbaviskar07@gmail.com>
2023-08-16 12:59:02 +0530

    Git Pushed

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'predico',
         'USER': 'postgres',
-        'PASSWORD': '12345678',
+        'PASSWORD': '1234',
         'HOST': 'localhost'
     }
 }
